fac957dd5739c6f1a57509c8aa467b976a4f8202
eg5or <eg5or@mail.ru>
2020-08-28 11:42:50 +0300

    16 lesson

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
             <div className={classes.item}>
                 <img src='https://cs8.pikabu.ru/avatars/1165/x1165542-515700605.png'/>
-                post 1
+                {props.message}
                 <div>
                 <span>like</span>
                 </div>
