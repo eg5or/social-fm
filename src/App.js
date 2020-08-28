754a4a1327cc6ffa1c7e61b4e075eac834e569cf
eg5or <eg5or@mail.ru>
2020-08-28 13:55:07 +0300

    20 lesson

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/Navbar/Navbar.jsx';
 import Profile from './components/Profile/Profile.jsx';
 import Dialogs from './components/Dialogs/Dialogs';
 import {BrowserRouter,Route} from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 
 const App = () => {
@@ -16,6 +19,9 @@ const App = () => {
             <div className='app-wrapper-content'>
                 <Route path='/dialogs' component={Dialogs} />
                 <Route path='/profile' component={Profile} />
+                <Route path='/news' component={News} />
+                <Route path='/music' component={Music} />
+                <Route path='/settings' component={Settings} />
             </div>
         </div>
         </BrowserRouter>
