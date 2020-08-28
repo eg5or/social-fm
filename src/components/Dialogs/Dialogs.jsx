97afbc60456d798b67fe9994b478d3250518a2f6
eg5or <eg5or@mail.ru>
2020-08-28 15:56:45 +0300

    25 lesson

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,30 +19,34 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Alina'},
         {id: 2, name: 'Temich'},
         {id: 3, name: 'Skvor'},
         {id: 4, name: 'Vanya'}
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'Kuku'},
         {id: 4, message: 'Yo'}
     ]
 
+    let dialogsElements = dialogs
+        .map(d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = messages
+        .map(m => <Message message={m.message}/>)
+
     return (
         <div>
             <div className={classes.dialogs}>
                 <div className={classes.dialogsItems}>
-                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    { dialogsElements }
                 </div>
                 <div className={classes.messages}>
-                    <Message message={messagesData[0].message} />
-                    <Message message={messagesData[1].message} />
+                    { messagesElements }
                 </div>
             </div>
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,12 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post';
 
 const MyPosts = () => {
-    let postsData = [
+    let posts = [
         {id: 1, message: 'Hi? how are you?', likesCount: '0'},
         {id: 2, message: 'Its my first post', likesCount: '23'}
     ]
+
+    let postsElements = posts.map (p => <Post message={p.message} likesCount={p.likesCount} />)
 
     return (
         <div className={classes.postsBlock}>
@@ -18,8 +20,7 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
+                { postsElements }
             </div>
         </div>
     )
