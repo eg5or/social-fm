3b9658919396e706fc068be8828008939ed794f5
eg5or <eg5or@mail.ru>
2020-08-28 13:30:18 +0300

    19 lesson

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Profile from './components/Profile/Profile.jsx';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter,Route} from 'react-router-dom';
 
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Dialogs />
+                <Route path='/dialogs' component={Dialogs} />
+                <Route path='/profile' component={Profile} />
             </div>
-
-            {/*<Profile />*/}
-
         </div>
+        </BrowserRouter>
     );
 }
 
