598e995759b768b700c415f05442adc456a07d3c
eg5or <eg5or@mail.ru>
2020-09-04 13:49:05 +0300

    38 lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,21 +9,24 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        props.dispatch({ type: 'ADD-POST' });
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        props.dispatch(action)
     }
 
     return (
         <div className={classes.postsBlock}>
             <h3>My Posts</h3>
             <div>
-                <div><textarea onChange={onPostChange}
+                <div>
+                    <textarea onChange={onPostChange}
                                ref={newPostElement}
-                               value={props.newPostText}/></div>
+                               value={props.newPostText}/>
+                </div>
                 <div>
                     <button onClick={ addPost }>Add post</button>
                 </div>
--- src/redux/state.js
+++ src/redux/state.js
@@ -22,28 +22,30 @@ let store = {
             ],
         }
     },
-    getState() {
-       return this._state;
-    },
     _callSubscriber() {
         console.log('State was changed');
     },
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    },
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
+    getState() {
+        return this._state;
     },
     subscribe(observer) {
         this._callSubscriber = observer; // паттерн - наблюдатель observer ИЗУЧИТЬ!!! (addEventListener
+    },
+
+    dispatch(action) { // { type: 'ADD-POST', message: 'text'}
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
     }
 }
 
