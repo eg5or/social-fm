0cb80db58862b81aae77c2895c1073806a3a860b
eg5or <eg5or@mail.ru>
2020-09-04 14:14:31 +0300

    39 lesson

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -49,6 +52,8 @@ let store = {
     }
 }
 
+export const addPostActionCreator = () => ({ type: ADD_POST })
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
 
 export default store;
 window.store = store;
