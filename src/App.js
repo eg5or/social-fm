1877de4647fd6c372c33932e87a0efbf25a2770c
eg5or <eg5or@mail.ru>
2020-09-07 13:22:39 +0300

    43 lesson

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Profile from './components/Profile/Profile.jsx';
-import Dialogs from './components/Dialogs/Dialogs';
 import {Route} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 
 const App = (props) => {
@@ -16,10 +16,8 @@ const App = (props) => {
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Route path='/dialogs' render={ () => <Dialogs store={props.store} /> } />
-                <Route path='/profile' render={ () => <Profile
-                    profilePage={props.state.profilePage}
-                    dispatch={props.dispatch} /> } />
+                <Route path='/dialogs' render={ () => <DialogsContainer store={props.store} /> } />
+                <Route path='/profile' render={ () => <Profile store={props.store} /> } />
                 <Route path='/news' render={ () => <News /> } />
                 <Route path='/music' render={ () => <Music /> } />
                 <Route path='/settings' render={ () => <Settings /> } />
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,14 +11,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action)
+        props.updateNewPostText(text);
     }
 
     return (
@@ -31,7 +30,7 @@ const MyPosts = (props) => {
                                value={props.newPostText}/>
                 </div>
                 <div>
-                    <button onClick={ addPost }>Add post</button>
+                    <button onClick={ onAddPost }>Add post</button>
                 </div>
             </div>
             <div className={classes.posts}>
