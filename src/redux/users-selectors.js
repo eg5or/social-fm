c3c265b1bcb778a0ffa145cba5f5a1d7be706411
eg5or <eg5or@mail.ru>
2020-09-26 19:26:41 +0300

    82 lesson

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,16 @@
-export const getUsers = (state) => {
+import {createSelector} from "reselect";
+
+export const getUsers = (state) => { // примитивный селектор
     return state.usersPage.users
 }
+
+export const getUsersSelector = (state) => {
+    return getUsers(state).filter(u => true)  // используем примитивный селектор с фильтрацией
+}
+
+export const getUsersSuperSelector = createSelector(getUsers, (users) => {
+    return users.filter(u => true)  // первым параметром передаем примитивный селектор, чтобы его результат вернуть в его функцию
+})
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize
